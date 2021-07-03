5c44f8d175b2df08d07d42ef2f6440e2cd21106f
Rocks-3601 <86807474+Rocks-3601@users.noreply.github.com>
2021-07-02 21:38:30 -0500

    Update game4.js

--- game4.js
+++ game4.js
@@ -1,10 +1,10 @@
 function setup(){
      createCanvas(400, 400);
+     background(12, 50, 200);
 }
 
 function draw(){
      fill(255, 125, 0);
      noStroke();
-     ellipse(200, 200, 50);
-     background(12, 50, 200);     
+     ellipse(200, 200, 50);     
 }
