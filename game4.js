0242a02a1fe62f5c229b542914531c9b485c2127
Rocks-3601 <rocks360123@gmail.com>
2021-07-03 09:37:54 -0500

    Update game4.js

--- game4.js
+++ game4.js
@@ -15,6 +15,18 @@ function draw(){
      ellipse(x, y, size);
      x = x + xspeed;
      y = y + yspeed;
+     if (x >= 400 - size){
+          x = x - xspeed;
+     }else if (x <= 0 + size){
+          x = x + xspeed;
+     }
+
+     if (y >= 400 - size){
+          y = y - yspeed;
+     }else if (x <= 0 + size){
+          y = y + ypeed;
+     }
+     
      if (mouseClicked()){
           xspeed = random(1, 5);
           yspeed = random(1, 5);
