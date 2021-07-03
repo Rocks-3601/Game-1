ba4eb6e00c253d5398274c535483ca90539e4996
Rocks-3601 <rocks360123@gmail.com>
2021-07-02 22:00:02 -0500

    Update game4.js

--- game4.js
+++ game4.js
@@ -1,10 +1,21 @@
 function setup(){
      createCanvas(400, 400);
+     // vars
+     var xspeed = 2;
+     var yspeed = 3;
+     var x = 200;
+     var y = 200;
+     var size = 50
 }
 
 function draw(){
      background(12, 50, 200);
      fill(255, 125, 0);
      noStroke();
-     ellipse(200, 200, 50);     
+     ellipse(x, y, 50);
+     x = x + xspeed;
+     y = y + yspeed;
+     if (mouseClicked()){
+          
+     }
 }
